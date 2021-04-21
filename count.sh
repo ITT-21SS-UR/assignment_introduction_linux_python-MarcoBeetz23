a7a41840fa5bc45740873faf3dd53c7189fd7197
MarcoBeetz23 <57911367+MarcoBeetz23@users.noreply.github.com>
2021-04-21 19:45:48 +0200

    Update count.sh
    idea to filter within tr by using '[:alnum:]' from
    
    https://unix.stackexchange.com/questions/41479/find-n-most-frequent-words-in-a-file

--- count.sh
+++ count.sh
@@ -1,1 +1,12 @@
+#!/bin/bash
 
+
+#get the text and rename it to "readme_text"
+wget -O readme_text ftp://sunsite.informatik.rwth-aachen.de/pub/mirror/ibiblio/gnome/README
+
+
+#every word in separate line and lower-case
+cat readme_text | tr " " "\n" | tr A-Z a-z
+
+#select all words and count quantity, show 10 most frequent words and delete the number
+cat readme_text | tr -c '[:alnum:]' '[\n*]' | sort | uniq -c | sort -nr | head -10 | tr -d 0-9
